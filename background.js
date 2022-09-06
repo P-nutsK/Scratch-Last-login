1ccb920a97ef47d39af4d517738416c5ecce5463
P_nutsK <107553542+P-nutsK@users.noreply.github.com>
2022-09-06 19:56:45 +0900

    'Bug fix'
    自動更新がうまくいかないバグを修正

--- background.js
+++ background.js
@@ -1,1 +1,9 @@
 chrome.runtime.onInstalled.addListener(chrome.runtime.openOptionsPage)
+chrome.runtime.onMessage.addListener(function(request) {
+	switch(request.type) {
+		case("openURL"): {
+			chrome.tabs.create({url:request.url});
+			break;
+		}
+	}
+})
