70bad6bc6addf9fb5e8671a4a68c1c0903d0e9a6
arch-herobrine <112319383+arch-herobrine@users.noreply.github.com>
2022-12-20 11:28:33 +0900

    Add files via upload

--- options.js
+++ options.js
@@ -8,34 +8,32 @@ var ignore;
 
 htmlBio.checked == true;
 chrome.storage.local.get(value => {
-    if(value.terget === "status") {
+    if (value.terget === "status") {
         htmlStatus.checked = true;
     } else {
         htmlBio.checked = true;
     }
-    if(value.data) htmlData.innerHTML = value.data.replaceAll("\\n","&#13;");
+    if (value.data) htmlData.innerHTML = value.data.replaceAll("\\n", "&#13;");
     console.log(JSON.stringify(value.ignore))
-    if(!JSON.stringify(value.ignore) == "[]"){
-        htmlIgnore.innerHTML = JSON.stringify(value.ignore).replaceAll("[","").replaceAll("]","").replaceAll(" ","").replaceAll(",","&#13;")
-    }
+    htmlIgnore.innerHTML = JSON.stringify(value.ignore).replaceAll("[", "").replaceAll("]", "").replaceAll(" ", "").replaceAll(",", "&#13;").replaceAll("\"", "")
     htmlCount.innerText = `残り約${200 - [...test(htmlData.value)].length}文字`;
 })
-htmlData.addEventListener("keyup",() => {
+htmlData.addEventListener("keyup", () => {
     htmlCount.innerText = `残り約${200 - [...test(htmlData.value)].length}文字`;
 })
-document.querySelector("#save").addEventListener("click",function() {
-    if(!htmlData.value) {
+document.querySelector("#save").addEventListener("click", function () {
+    if (!htmlData.value) {
         htmlCount.textContent = "本文がありません";
         return;
     }
-    if(!htmlIgnore.value.replaceAll("\n","")){
+    if (!htmlIgnore.value.replaceAll("\n", "")) {
         ignore = []
-    }else{
+    } else {
         ignore = htmlIgnore.value.split("\n")
     }
     chrome.storage.local.set({
         terget: htmlBio.checked === true ? "bio" : "status",
-        data: htmlData.value.replaceAll("\n","\\n"),
+        data: htmlData.value.replaceAll("\n", "\\n"),
         ignore: ignore
     })
     htmlCount.textContent = "saved!";
@@ -43,21 +41,21 @@ document.querySelector("#save").addEventListener("click",function() {
 
 function test(str) {
     const date = new Date();
-    return str.replace(/%ALL/g,date.toLocaleString())
-	.replace(/%YE/g, date.getFullYear())
-	.replace(/%MO/g, date.getMonth() + 1)
-	.replace(/%DA/g, date.getDate())
-	.replace(/%HO/g, date.getHours())
-	.replace(/%MI/g, date.getMinutes())
-	.replace(/%SE/g, date.getSeconds())
-	.replace(/%MS/g, date.getMilliseconds())
-	.replace(/%UALL/g,date.toUTCString)
-	.replace(/%UYE/g, date.getUTCFullYear())
-	.replace(/%UMO/g, date.getUTCMonth() + 1)
-	.replace(/%UDA/g, date.getUTCDate())
-	.replace(/%UHO/g, date.getUTCHours())
-	.replace(/%UMI/g, date.getUTCMinutes())
-	.replace(/%USE/g, date.getUTCSeconds())
-	.replace(/%UMS/g, date.getUTCMilliseconds())
-    .replace(/%ISO/g, date.toISOString())
+    return str.replace(/%ALL/g, date.toLocaleString())
+        .replace(/%YE/g, date.getFullYear())
+        .replace(/%MO/g, date.getMonth() + 1)
+        .replace(/%DA/g, date.getDate())
+        .replace(/%HO/g, date.getHours())
+        .replace(/%MI/g, date.getMinutes())
+        .replace(/%SE/g, date.getSeconds())
+        .replace(/%MS/g, date.getMilliseconds())
+        .replace(/%UALL/g, date.toUTCString)
+        .replace(/%UYE/g, date.getUTCFullYear())
+        .replace(/%UMO/g, date.getUTCMonth() + 1)
+        .replace(/%UDA/g, date.getUTCDate())
+        .replace(/%UHO/g, date.getUTCHours())
+        .replace(/%UMI/g, date.getUTCMinutes())
+        .replace(/%USE/g, date.getUTCSeconds())
+        .replace(/%UMS/g, date.getUTCMilliseconds())
+        .replace(/%ISO/g, date.toISOString())
 }
